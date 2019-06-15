9d6892cd45a12df30a3def1b45962adb7e3e14f5
John Gregory Funtera <gregory.funtera@gmail.com>
2019-06-15 17:22:34 +0800

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { ProjectsRoutingModule } from './routing/projects-routing.module';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProjectCreateComponent } from './projects/project-create/project-create.component';
+import { ProjectViewComponent } from './projects/project-view/project-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    ProjectCreateComponent,
+    ProjectViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ProjectsRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routing/projects-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from '../auth/auth.component';
+import { HomeComponent } from '../home/home.component';
+import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
+import { ProjectViewComponent } from '../projects/project-view/project-view.component';
+import { ProjectCreateComponent } from '../projects/project-create/project-create.component';
+
+const routes: Routes = [
+  { path: 'login', component: AuthComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'projects/:id', component: ProjectViewComponent },
+  { path: 'projects/create', component: ProjectCreateComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class ProjectsRoutingModule { }
